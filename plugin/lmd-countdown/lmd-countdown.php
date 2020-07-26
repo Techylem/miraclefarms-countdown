a1901f4f7d2d5908726fdd80667915b6e353a7c5
Aamir7373 <aamerali1355@gmail.com>
2020-07-26 19:40:44 +0500

    Custom Class field added

--- plugin/lmd-countdown/lmd-countdown.php
+++ plugin/lmd-countdown/lmd-countdown.php
@@ -15,7 +15,8 @@
 		add_shortcode('COUNTDOWN', array( $this, 'lmd_countdown' ));
 		add_action('wp_enqueue_scripts', array( $this, 'lmd_enque_styles' ));
 		add_action('admin_menu', array( $this, 'lmd_register_options_page' ));
-		add_action( 'admin_init', array( $this, 'lmd_register_settings' ) );
+		add_action( 'admin_init', array( $this, 'lmd_register_title' ) );
+		add_action( 'admin_init', array( $this, 'lmd_register_class' ) );
 	}
 
 	public function lmd_enque_styles(){
@@ -27,9 +28,14 @@
 	  add_options_page('Page Title', 'Coundown Settings', 'manage_options', 'lmd_countdown_settings', array($this, 'myplugin_options_page'));
 	}
 
-	public function lmd_register_settings() {
-	   add_option( 'countdown_title', 'This is my option value.');
+	public function lmd_register_title() {
+	   add_option( 'countdown_title', 'This is title my option value.');
 	   register_setting( 'lmd_options_count', 'countdown_title' );
+	}
+
+	public function lmd_register_class() {
+	   add_option( 'countdown_class', 'This is class option value.');
+	   register_setting( 'lmd_options_class', 'countdown_class' );
 	}
 
 	public function myplugin_options_page(){
@@ -39,10 +45,15 @@
 		  <h2>Add Countdown Settings</h2>
 		  <form method="post" action="options.php">
 		  <?php settings_fields( 'lmd_options_count' ); ?>
+		  <?php settings_fields( 'lmd_options_class' ); ?>
 		  <table>
 		  <tr valign="top">
 		  <th scope="row"><label for="countdown_title">Add Title</label></th>
 		  <td><input type="text" id="countdown_title" name="countdown_title" value="<?php echo get_option('countdown_title'); ?>" /></td>
+		  </tr>
+		  <tr valign="top">
+		  	<th scope="row"><label for="countdown_class">Add Custom Class</label></th>
+		  	<td><input type="text" id="countdown_class" name="countdown_class" value="<?php echo get_option('countdown_class'); ?>" /></td>
 		  </tr>
 		  </table>
 		  <?php  submit_button(); ?>
